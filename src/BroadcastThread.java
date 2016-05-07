9e53dec5cf5bcbd6677af490dd7daf29ba01afbb
linii <lining.wang@yale.edu>
2016-05-07 14:37:10 -0400

    moved broadcast thread to own file

---
+++ src/BroadcastThread.java
@@ -0,0 +1,75 @@
+import java.io.DataOutputStream;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.UUID;
+import java.util.concurrent.Callable;
+
+class BroadcastThread implements Callable {
+    private final Servent servent;
+    private final InetAddress neighbor;
+    private final String filename;
+
+
+    public BroadcastThread(Servent servent, InetAddress neighbor, String filename, ArrayList<String> files) {
+        this.servent = servent;
+        this.neighbor = neighbor;
+        this.filename = filename;
+    }
+
+    public String call() {
+        Debug.DEBUG("Running broadcast thread", "call");
+        if (neighbor == null || filename == null || servent == null)
+            return null;
+
+        sendQuery();
+
+        //wait until the file no longer is being looked for (means it was downloaded successfully)
+        Debug.DEBUG("Function finished", "call");
+        synchronized (ClientThread.files) {
+            while (!ClientThread.files.isEmpty()) {
+                try {
+                    ClientThread.files.wait();
+                } catch (InterruptedException ex) {
+                    System.out.println("Waiting for pool interrupted.");
+                    ex.printStackTrace();
+                }
+            }
+            return null;
+        }
+    }
+
+    private void sendQuery() {
+        sendQuery(filename, neighbor);
+    }
+
+    private void sendQuery(String filename, InetAddress neighbor) {
+        Debug.DEBUG("Sending query for " + neighbor, "sendQuery");
+
+        UUID descriptorID = UUID.randomUUID();
+        servent.arr.add(descriptorID, GnutellaPacket.QUERY, null);
+        byte[] payload = Utility.stringToByteArray(filename);
+        GnutellaPacket queryPacket =
+                new GnutellaPacket(descriptorID, GnutellaPacket.QUERY, GnutellaPacket.DEF_TTL, 0, payload);
+        sendPacket(neighbor, servent.getQUERYPORT(), queryPacket);
+    }
+
+    private void sendPacket(InetAddress to, int port, GnutellaPacket pkt) {
+        Debug.DEBUG_F("Sending packet to " + to.getCanonicalHostName()
+                + ":" + pkt.toString(), "sendPacket");
+
+        try {
+            Socket s = new Socket(to, port);
+            DataOutputStream out = new DataOutputStream(s.getOutputStream());
+
+            Debug.DEBUG("Packet length: " + pkt.pack().length, "sendPacket");
+            out.writeInt(pkt.pack().length);
+            out.write(pkt.pack());
+            out.flush();
+            Debug.DEBUG("Wrote the packet", "sendPacket");
+            out.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
--- src/ClientThread.java
+++ src/ClientThread.java
@@ -39,7 +39,6 @@ class ClientThread extends Thread {
 
             String[] words = nextLine.trim().split("\\s+");
             if (words.length == 0 && words[0].toUpperCase().equals("BYE")) {
-                // close the connection
                 closeConnection();
             }
             for (String word : words) {
@@ -111,74 +110,7 @@ class ClientThread extends Thread {
     }
 }
 
-class BroadcastThread implements Callable {
-    private final Servent servent;
-    private final InetAddress neighbor;
-    private final String filename;
 
-
-    public BroadcastThread(Servent servent, InetAddress neighbor, String filename, ArrayList<String> files) {
-        this.servent = servent;
-        this.neighbor = neighbor;
-        this.filename = filename;
-    }
-
-    public String call() {
-        Debug.DEBUG("Running broadcast thread", "call");
-        if (neighbor == null || filename == null || servent == null)
-            return null;
-
-        sendQuery();
-
-        //wait until the file no longer is being looked for (means it was downloaded successfully)
-        Debug.DEBUG("Function finished", "call");
-        synchronized (ClientThread.files) {
-            while (!ClientThread.files.isEmpty()) {
-                try {
-                    ClientThread.files.wait();
-                } catch (InterruptedException ex) {
-                    System.out.println("Waiting for pool interrupted.");
-                    ex.printStackTrace();
-                }
-            }
-            return null;
-        }
-    }
-
-    private void sendQuery() {
-        sendQuery(filename, neighbor);
-    }
-
-    private void sendQuery(String filename, InetAddress neighbor) {
-        Debug.DEBUG("Sending query for " + neighbor, "sendQuery");
-
-        UUID descriptorID = UUID.randomUUID();
-        servent.arr.add(descriptorID, GnutellaPacket.QUERY, null);
-        byte[] payload = Utility.stringToByteArray(filename);
-        GnutellaPacket queryPacket =
-                new GnutellaPacket(descriptorID, GnutellaPacket.QUERY, GnutellaPacket.DEF_TTL, 0, payload);
-        sendPacket(neighbor, servent.getQUERYPORT(), queryPacket);
-    }
-
-    private void sendPacket(InetAddress to, int port, GnutellaPacket pkt) {
-        Debug.DEBUG_F("Sending packet to " + to.getCanonicalHostName()
-                + ":" + pkt.toString(), "sendPacket");
-
-        try {
-            Socket s = new Socket(to, port);
-            DataOutputStream out = new DataOutputStream(s.getOutputStream());
-
-            Debug.DEBUG("Packet length: " + pkt.pack().length, "sendPacket");
-            out.writeInt(pkt.pack().length);
-            out.write(pkt.pack());
-            out.flush();
-            Debug.DEBUG("Wrote the packet", "sendPacket");
-            out.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-}
 
 
 
